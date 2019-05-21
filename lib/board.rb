09b5ca3d07520c50daf915bf4abcbc0880947380
the3rdheat <rockstrongo22@gmail.com>
2019-05-20 22:20:22 -0700

    Implement Board make move method

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,53 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1=name1
+    @name2=name2
+    @cups=Array.new(14){Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    n=[:stone,:stone,:stone,:stone]
+    (0...6).each do |i|
+
+      @cups[i]+=n.dup
+    end
+    (7...13).each do |i|
+      @cups[i]+=n.dup
+    end 
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+    # raise "Invalid starting cup" if start_pos <0 || start_pos>12
+#    raise "That isn't a valid cup" if start_pos <0 || start_pos>12
+    # raise "Starting cup is empty" if start_pos.empty?
+#    raise "Thats your scoring cup" if start_pos.empty?
   end
 
+
   def make_move(start_pos, current_player_name)
+
+    curCup=start_pos
+    hand=@cups[start_pos].dup
+    @cups[curCup]=[]
+
+    while !hand.empty? 
+      curCup+=1
+      if current_player_name==@name1&&curCup==13
+        curCup=0
+      end
+      if current_player_name==@name2 && curCup==6
+        curCup=7
+      end
+      @cups[curCup]<<hand.shift
+    end
+    render
+    next_turn(curCup)
+
   end
 
   def next_turn(ending_cup_idx)
